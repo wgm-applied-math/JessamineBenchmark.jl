2193296eacc204c214ca10d0ebc4e42cfd2bd977
W. Garrett Mitchener <mitchenerg@charleston.edu>
2026-05-21 14:20:39 -0400

    Template tool for getting started
    Requires PkgTemplates

---
+++ Tools/GetStarted.jl
@@ -0,0 +1,33 @@
+module GetStarted
+
+using PkgTemplates
+
+t = Template(
+    user = "wgmitchener",
+    authors = ["W. Garrett Mitchener <mitchenerg@charleston.edu> and others"],
+    dir = ".",
+    julia = v"1.12.6",
+    plugins = [
+        License(
+            name = "GPL-3.0-or-later",
+        ),
+        Git(
+            name = "W. Garrett Mitchener",
+            email="mitchenerg@charleston.edu",
+            ssh = true,
+            jl = true,
+        ),
+        Documenter{GitHubActions}(
+            #user = "wgmitchener",
+        ),
+        Formatter(
+            style = "sciml",
+        )
+    ]
+)
+
+function (@main)(args = ARGS)
+    t("JessamineBenchmark.jl")
+end
+
+end
