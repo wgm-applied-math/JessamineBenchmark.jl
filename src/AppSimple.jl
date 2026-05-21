d8b98e3bd76a234b47c88be5438f461bd66f9577
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-05-21 18:37:23 -0400

    Getting started

--- src/AppSimple.jl
+++ src/AppSimple.jl
@@ -1,12 +1,49 @@
 # To run this CLI app manually:
-# julia --project=@. -m JessamineSciKitLearn.AppSimple
+# julia --project=@. -m JessamineBenchmnark.AppSimple
 
 module AppSimple
 
-using ..JessamineSciKitLearn
+using ArgParse
+using CSV
+using DataFrames
+
+using JessamineSciKitLearn
+
+s = ArgParseSettings()
+
+@add_arg_table! s begin
+    ["--rng-seed"],
+    begin
+	help = "seed for random number generator"
+        arg_type = UInt64
+        default = 0x677e87db530bd280
+    end,
+    ["--data-file"],
+    begin
+        help = "file with data table"
+        arg_type = String
+        required = true
+    end
+end
 
 function (@main)(args = ARGS)
-
+    parsed_args = parse_args(args, s)
+    data_file = parsed_args["data-file"]
+    df = load_data_file(data_file)
+    @show df
+    return 0
 end
+
+function load_data_file(path)
+    # Choose a delimiter based on the file name
+    if occursin(".tsv", path)
+        return CSV.read(path, DataFrame, delim = '\t')
+    elseif occursin(".csv", path)
+        return CSV.read(path, DataFrame, delim = ',')
+    else
+        return CSV.read(path, DataFrame)
+    end
+end
+
 
 end
