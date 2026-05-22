5d01884a1bcbd9c9cfe854e47de2d226753d1636
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-05-21 21:13:25 -0400

    Getting started on AppSimple sampler

--- src/AppSimple.jl
+++ src/AppSimple.jl
@@ -1,5 +1,5 @@
 # To run this CLI app manually:
-# julia --project=@. -m JessamineBenchmnark.AppSimple
+# julia --project=@. -m JessamineBenchmark.AppSimple
 
 module AppSimple
 
@@ -9,10 +9,12 @@ using DataFrames
 
 using JessamineSciKitLearn
 
-s = ArgParseSettings()
+# autofix_names = true: Means an option like --random-state
+# results in a dictionary item with key "random_state".
+s = ArgParseSettings(autofix_names = true)
 
 @add_arg_table! s begin
-    ["--rng-seed"],
+    ["--random-state"],
     begin
 	help = "seed for random number generator"
         arg_type = UInt64
@@ -23,14 +25,33 @@ s = ArgParseSettings()
         help = "file with data table"
         arg_type = String
         required = true
+    end,
+    ["--op-inventory"],
+    begin
+	help = "operation inventory"
+        arg_type = String
+        default = "Polynomial"
     end
+
 end
 
 function (@main)(args = ARGS)
+    # Load the data file
     parsed_args = parse_args(args, s)
-    data_file = parsed_args["data-file"]
+    data_file = parsed_args["data_file"]
     df = load_data_file(data_file)
-    @show df
+
+    # Extract the input and output columns
+    output_column = :label
+    y = df[!, output_column]
+    X = df[!, Not(output_column)]
+
+    # Feed it to JessamineSciKitLearn
+
+    result = regression_main(X, y, parsed_args)
+
+    @info "Result is $result"
+
     return 0
 end
 
@@ -41,6 +62,7 @@ function load_data_file(path)
     elseif occursin(".csv", path)
         return CSV.read(path, DataFrame, delim = ',')
     else
+        # Choose automatically
         return CSV.read(path, DataFrame)
     end
 end
