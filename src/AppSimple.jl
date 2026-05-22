4b0a835da726001304cc8732b9407bd67087f105
W. Garrett Mitchener <mitchenerg@charleston.edu>
2026-05-22 18:05:43 -0400

    Store results in JSON

--- src/AppSimple.jl
+++ src/AppSimple.jl
@@ -6,8 +6,10 @@ module AppSimple
 using ArgParse
 using CSV
 using DataFrames
-
+using Dates
+using JSON
 using JessamineSciKitLearn
+using JessamineSciKitLearn: @cfield
 
 # autofix_names = true: Means an option like --random-state
 # results in a dictionary item with key "random_state".
@@ -20,25 +22,41 @@ s = ArgParseSettings(autofix_names = true)
         arg_type = UInt64
         default = 0x677e87db530bd280
     end,
-    ["--data-file"],
+    ["--data-file", "-d"],
     begin
         help = "file with data table"
         arg_type = String
         required = true
+    end,
+    ["--output-file", "-o"],
+    begin
+	help = "output file path"
+        arg_type = String
     end,
     ["--op-inventory"],
     begin
 	help = "operation inventory"
         arg_type = String
         default = "Polynomial"
+    end,
+    ["--max-time"],
+    begin
+	help = "maximum time in seconds"
+        arg_type = Int
+        default = nothing
     end
-
 end
 
 function (@main)(args = ARGS)
+    prespec = parse_args(args, s)
+
+    # Set time limit
+    @cfield prespec max_time 30
+    stop_deadline = now() + Dates.Second(max_time)
+    prespec["stop_deadline"] = stop_deadline
+
     # Load the data file
-    parsed_args = parse_args(args, s)
-    data_file = parsed_args["data_file"]
+    data_file = prespec["data_file"]
     df = load_data_file(data_file)
 
     # Extract the input and output columns
@@ -48,9 +66,20 @@ function (@main)(args = ARGS)
 
     # Feed it to JessamineSciKitLearn
 
-    result = regression_main(X, y, parsed_args)
+    result = regression_main_detailed(X, y, prespec)
 
     @info "Result is $result"
+
+    short_result = (
+        best_agent = result.best_agent,
+        genome_spec = result.genome_spec,
+        y_num_str = result.y_num_str)
+
+    output_file = get(prespec, "output_file", nothing)
+    if !isnothing(output_file)
+        @info "Writing to $output_file"
+        JSON.json(output_file, short_result)
+    end
 
     return 0
 end
