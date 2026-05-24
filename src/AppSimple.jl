6f0cede7d7d7bc9f7b9f26ac21b004e7c7c4f46c
W. Garrett Mitchener <mitchenerg@charleston.edu>
2026-05-24 19:20:48 -0400

    Clean up specification of the argument parser

--- src/AppSimple.jl
+++ src/AppSimple.jl
@@ -10,46 +10,80 @@ using Dates
 using JSON
 using JessamineSciKitLearn
 using JessamineSciKitLearn: @cfield
+using Pkg
+using TOML
 
 include("ArgsJessamine.jl")
 
 # autofix_names = true: Means an option like --random-state
 # results in a dictionary item with key "random_state".
-s = ArgParseSettings(autofix_names = true, add_version = true)
+s = ArgParseSettings(
+    autofix_names = true,
+    add_version = true,
+    version = string(Pkg.project().version))
 
 args_overall = [
-    ["--random-state"],
+    ["--config-file"],
     Dict(
-        :help => "seed for random number generator",
-        :arg_type => UInt64,
-        :default => 0x677e87db530bd280
+        :help => "read configuration from a TOML file; can be repeated",
+        :arg_type => String,
+        :action => :append_arg
     ),
     ["--data-file", "-d"],
     Dict(
         :help => "file with data table",
         :arg_type => String,
-        :required => true
-    ),
-    ["--max-time"],
-    Dict(
-	:help => "maximum time in seconds",
-        :arg_type => Int,
-        :default => nothing
     ),
 ]
 
 args_output = [
-    ["--output-file", "-o"],
+    ["--output-file"],
     Dict(
 	:help => "output file path",
         :arg_type => String
-    )
+    ),
+    ["--config-dump-file"],
+    Dict(
+        :help => "save the overall argument configuration to a TOML file",
+        :arg_type => String,
+    ),
 ]
 
 add_arg_table!(s, args_overall..., args_output..., args_jessamine...)
 
 function (@main)(args = ARGS)
-    prespec = parse_args(args, s)
+    prespec_original = parse_args(args, s)
+
+    # Get rid of any nothings, they just cause trouble.
+    prespec = filter(p -> !isnothing(p.second), prespec_original)
+
+
+    # Load config files
+    if haskey(prespec, "config_file")
+        config_files = prespec["config_file"]
+        for config_file in config_files
+            cf = TOML.parsefile(config_file)
+            @info "Loaded $(config_file): $cf"
+            # Merge so that args on the command line supersede
+            # what's in a file.
+            prespec = merge(cf, prespec)
+            @info "Prespec is now $prespec"
+        end
+        @info "After loading config files, prespec is $prespec"
+    end
+
+    # Maybe store configuration
+    dump_file = get(prespec, "config_dump_file", nothing)
+    if !isnothing(dump_file)
+        # It doesn't make sense to include the config_dump_file
+        # key in the dump file, or the config_file array, so take them out.
+        prespec_fixed = copy(prespec)
+        delete!(prespec_fixed, "config_dump_file")
+        delete!(prespec_fixed, "config_file")
+        open(dump_file, "w") do io
+            TOML.print(io, prespec_fixed)
+        end
+    end
 
     # Set time limit
     @cfield prespec max_time 30
@@ -57,7 +91,10 @@ function (@main)(args = ARGS)
     prespec["stop_deadline"] = stop_deadline
 
     # Load the data file
-    data_file = prespec["data_file"]
+    data_file = get(prespec, "data_file", nothing)
+    if isnothing(data_file)
+        error("No data file specified")
+    end
     df = load_data_file(data_file)
 
     # Extract the input and output columns
--- src/ArgsJessamine.jl
+++ src/ArgsJessamine.jl
@@ -1,8 +1,136 @@
-args_jessamine = [
+args_search = [
+    ["--random-state"],
+    Dict(
+        :help => "seed for random number generator",
+        :arg_type => UInt64,
+        :default => 0x677e87db530bd280
+    ),
+    ["--max-time"],
+    Dict(
+        :help => "maximum time in seconds",
+        :arg_type => Int64,
+    ),
+    # TODO: "stop_deadline": [dt.datetime, None],
+    ["--num-islands"],
+    Dict(
+        :help => "number of simultaneous islands per exploded specification",
+        :arg_type => Int64,
+    ),
+    # TODO: "stop_threshold": [float, None],
+    ["--simplify"],
+    Dict(
+        :help => "whether to run a simplification epoch",
+        :default => true
+    ),
+]
+
+args_genome_spec = [
+    ["--output-size"],
+    Dict(
+        :help => "genome output size",
+        :arg_type => Int64
+    ),
+    ["--scratch-size"],
+    Dict(
+        :help => "genome scratch size",
+        :arg_type => Int64
+    ),
+    ["--parameter-size"],
+    Dict(
+        :help => "genome parameter size",
+        :arg_type => Int
+    ),
+    ["--num-time-steps"],
+    Dict(
+        :help => "number of genome time steps",
+        :arg_type => Int64
+    ),
+]
+
+args_mutation_spec = [
     ["--op-inventory"],
     Dict(
-	:help => "operation inventory",
+        :help => "operation inventory",
         :arg_type => String,
         :default => "Polynomial"
-    )
+    ),
+    ["--p-mutate-op"],
+    Dict(
+        :help => "probability that an operation mutates",
+        :arg_type => Float64,
+    ),
+    ["--p-mutate-index"],
+    Dict(
+        :help => "probability that an operand index mutates",
+        :arg_type => Float64,
+    ),
+    ["--p-duplicate-index"],
+    Dict(
+        :help => "probability that an operand index is duplicated",
+        :arg_type => Float64,
+    ),
+    ["--p-delete-index"],
+    Dict(
+        :help => "probability that an operand index is deleted",
+        :arg_type => Float64,
+    ),
+    ["--p-duplicate-instruction"],
+    Dict(
+        :help => "probability that a whole instruction is duplicated",
+        :arg_type => Float64,
+    ),
+    ["--p-delete-instruction"],
+    Dict(
+        :help => "probability that a whole instruction is deleted",
+        :arg_type => Float64,
+    ),
+    ["--p-hop-instruction"],
+    Dict(
+        :help => "probability that an instruction hops to another block",
+        :arg_type => Float64,
+    ),
 ]
+
+args_selection_spec = [
+    ["--num-to-keep"],
+    Dict(
+        :help => "how many genomes to keep from one generation to the next (elitism)",
+        :arg_type => Int64
+    ),
+    ["--num-to-generate"],
+    Dict(
+        :help => "how many genomes to generate for next generation",
+        :arg_type => Int64
+    ),
+    ["--p-take-better"],
+    Dict(
+        :help => "probability that in a tournament, the better-rated genome is chosen as a parent",
+        :arg_type => Float64,
+    ),
+    ["--p-take-very-best"],
+    Dict(
+        :help => "probability that in a tournament, the best-rated genome in the current population is chosen as a parent",
+        :arg_type => Float64
+    ),
+]
+
+args_regularization = [
+    ["--lambda-b"],
+    Dict(
+        :help => "weight given to L2 norm of linear coefficients (ridge regression, Tikhonov regularization)",
+        :arg_type => Float64
+    ),
+    ["--lambda-p"],
+    Dict(
+        :help => "weight given to L2 norm of nonlinear model constants",
+        :arg_type => Float64
+    ),
+    ["--lambda-op"],
+    Dict(
+        :help => "weight given to total number of operands in genome",
+        :arg_type => Float64
+    ),
+]
+
+
+args_jessamine = [args_search; args_genome_spec; args_mutation_spec; args_selection_spec; args_regularization]
