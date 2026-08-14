d4c8d509e2e43c791ac15a03b32f5a61f928aed0
Documenter.jl <documenter@juliadocs.github.io>
2026-08-14 05:13:41 +0000

    build based on d9b7379

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#JessamineBenchmark","page":"Home","text":"Documentation for JessamineBenchmark.\n\n","title":"JessamineBenchmark"}]
+}
