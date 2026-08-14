fc528232bea9724c60d29c73ac4a35cfc7217be1
W. Garrett Mitchener <mitchenerg@charleston.edu>
2026-08-14 00:38:41 -0400

    Dammit I have to print-debug over GitHub...

--- docs/setup.jl
+++ docs/setup.jl
@@ -1,6 +1,11 @@
 using Pkg
+@info "Dev Jessamine.jl"
 Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/Jessamine.jl.git"))
+@info "Dev JessamineSymbolics.jl"
 Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/JessamineSymbolics.jl.git"))
+@info "Dev JessamineCLI.jl"
 Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/JessamineCLI.jl.git"))
+@info "Dev this package"
 Pkg.develop(PackageSpec(path=pwd()))
+@info "Instantiate"
 Pkg.instantiate()
